3177f384d759e09940038b8c8dc971fd78f46509
Dano <danielaherniv@gmail.com>
2019-11-22 23:11:26 -0500

    update

--- client/src/components/Logo/index.js
+++ client/src/components/Logo/index.js
@@ -4,7 +4,7 @@ import "./style.css"
 function Logo() {
     return (
         <div className="logo">
-            <img className="logo-position" src="../../public/tustin-bookmonth.jpg" alt="Book Icon" />
+            <img className="logo-position" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMpcrg5Al1tmTE2bA1kyIotHiHM25tXr5ZkIyynZyRabgthw98&s" alt="Book Icon" />
             <h1 className="logo-text logo-position google-book-search">Google Book Search </h1>
             <br />
 
