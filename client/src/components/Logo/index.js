43d53e2051e9cc3b2de822b8c8a485bc336c0310
Dano <danielaherniv@gmail.com>
2019-11-22 22:34:38 -0500

    update

--- client/src/components/Logo/index.js
+++ client/src/components/Logo/index.js
@@ -4,9 +4,10 @@ import "./style.css"
 function Logo() {
     return (
         <div className="logo">
-            <img className="logo-position" src="http://www.cliftonnoble.com/wp-content/uploads/2019/05/book-emoji.png" alt="Book Icon" />
+            <img className="logo-position" src="../../../public/tustin-bookmonth.jpg" alt="Book Icon" />
             <h1 className="logo-text logo-position google-book-search">Google Book Search </h1>
-            <h5 className="logo-text logo-position book-search-small">Built With React</h5>
+            <br />
+
         </div>
     )
 }
